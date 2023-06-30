edd97ac570dcf7b51d8471bcde8fb45bb993d5e5
Lorenzo Mokwa <lomokwa@Lorenzos-MacBook-Air.local>
2023-06-30 11:45:45 -0400

    added POST functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+// We know building in one component is not ideal, but props, context and useEffect have not been taught yet.
+// We're doing all in one component. 
+// Forgive me Father, for I have sinned.
 
 function App() {
+  const [candyList, setCandyList] = useState();
+
+  const getAllCandy = () => {
+    fetch("https://express-firestore-lmkw.web.app/candy")
+      .then(response => response.json())
+      .then(setCandyList)
+      .catch(alert)
+  }
+
+  const addNewCandy = (e) => {
+    if (!e.target.name.value || !e.target.price.value || !e.target.size.value) return;
+    e.preventDefault();
+    const newCandy = {
+      name: e.target.name.value,
+      size: e.target.size.value,
+      price: e.target.price.value,
+      calories: e.target.calories.value,
+    };
+    fetch("https://express-firestore-lmkw.web.app/candy", {
+      method: "POST", // if method not specified, defaults "GET"
+      headers: {"Content-type": "application/json",},
+      body: JSON.stringify(newCandy)
+    })
+      .then(response => response.json())
+      .then(setCandyList)
+      .catch(alert)
+      .finally(() => {
+        e.target.name.value = ""
+        e.target.size.value = ""
+        e.target.price.value = ""
+        e.target.calories.value = ""
+      })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
+      <h1>Candy Store</h1>
+
+      <h2>Add Candy</h2>
+      <form onSubmit={addNewCandy}>
+        <label htmlFor='name'>
+          Name: <input type='text' name='name' />
+        </label><br />
+        <label htmlFor='size'>
+          Size: <input type='text' name='size' />
+        </label><br />
+        <label htmlFor='price'>
+          Price: <input type='text' name='price' />
+        </label><br />
+        <label htmlFor='calories'>
+          Calories: <input type='number' name='calories' />
+        </label><br />
+        <input type='submit' value="Add Candy" />
+      </form>
+      
+      <h2>All the Candy
+        <button onClick={getAllCandy}>GET</button>
+      </h2>
+      <table>
+        <thead>
+          <td>Name</td>
+          <td>Size</td>
+          <td>Price</td>
+          <td>Cal</td>
+        </thead>
+        <tbody>
+          {candyList &&
+            candyList.map(candy => (
+              <tr key={candy.id}>
+                <td>{candy.name}</td>
+                <td>{candy.size}</td>
+                <td>${candy.price}</td>
+                <td>{candy.calories}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </main>
+  )
+};
 
 export default App;
